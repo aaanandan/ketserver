baef43a83b9df097bc6348285de87ee2abbd446d
anandan balasupramanian <anandan.bs@gmail.com>
2023-03-12 01:05:33 +0530

    attempts

--- routes/majorSubjectQuestions.mjs
+++ routes/majorSubjectQuestions.mjs
@@ -1,10 +1,12 @@
 import express from "express";
-import { getAllQuestions, getQuestion } from "./utils.mjs";
+import { getAllQuestions, getQuestion, updateUser, updateUserResponse, getUserAttempts } from "./utils.mjs";
 const router = express.Router();
 let results = null;
 // Get a all questions from hindu_history
 router.post("/getQuestions", async (req, res) => {
+  await updateUser(req);
   results = await getAllQuestions(req.body.major);
+
   let questions = results.map(res => { delete res.correct_answer; return res; })
   res.send(questions).status(200);
 });
@@ -13,9 +15,19 @@ router.post("/getQuestions", async (req, res) => {
 router.post("/validateAnswer", async (req, res) => {
   // if (!results) results = await getAllQuestions('hindu_history');
   // let answer = results.find(ele => ele._id === req.params._id);
+  // console.log(req.body);
   let result = await getQuestion(req);
+  await updateUserResponse(req, result.correct_answer);
   if (!result) res.send("Not found").status(404);
   else res.send(result).status(200);
 });
+
+router.post('/attempts', async (req, res) => {
+  // let result = await getQuestion(req);
+  let result = await getUserAttempts(req);
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+
+})
 
 export default router;
--- routes/utils.mjs
+++ routes/utils.mjs
@@ -4,7 +4,7 @@ import { ObjectId } from "mongodb";
 
 export async function getQuestion(req) {
     let collection = await db.collection(req.body.major);
-    let query = { _id: ObjectId(req.body._questionId) };
+    let query = { _id: ObjectId(req.body.question._questionId) };
     let result = await collection.findOne(query);
     return result;
 }
@@ -23,4 +23,55 @@ export function shuffle(array) {
         [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
-}; 
+};
+
+export async function getUserAttempts(req) {
+    let response = {
+        hinduPhilosophy: await getAttemptCount(req, 'hinduPhilosophy'),
+        hinduHistory: await getAttemptCount(req, 'hinduHistory'),
+        hinduIdentity: await getAttemptCount(req, 'hinduIdentity'),
+        knowYourNation: await getAttemptCount(req, 'knowYourNation')
+    }
+    console.log(response);
+    return response;
+}
+
+async function getAttemptCount(req, major) {
+    let collection = db.collection('quizUser');
+    let query = { email: req.body.email, major };
+    let options = { sort: { "index": -1 } };
+    let rec = await collection.findOne(query, options).catch(e => console.log(e));
+    if (rec) return rec.index;
+    return 0;
+}
+
+export async function updateUser(req) {
+    let collection = db.collection('quizUser');
+    let query = { email: req.body.email, major: req.body.major };
+    let options = { sort: { "index": -1 } };
+    let rec = await collection.findOne(query, options);
+    let index = rec ? rec.index + 1 : 1;
+    console.log(rec, index);
+    let doc = { name: req.body.name || '', email: req.body.email, major: req.body.major, index, response: [{}] };
+    await collection.insertOne(doc).then(() => {
+
+        console.log('added new attempt');
+    }).catch(e => {
+        console.log('e', e);
+    });
+}
+
+
+export async function updateUserResponse(req, ans) {
+    let collection = db.collection('quizUser');
+    let query = { email: req.body.email, major: req.body.major };
+    let sort = { sort: { "index": -1 } };
+    let question = req.body.question;
+    question.correct_answer = ans;
+    question.point = question.correct_answer === question.user_answer ? 1 : 0;
+    let response = [...req.body.questionsAndAnswers, question];
+    // console.log(response);
+    // // let rec = await collection.findOneAndUpdate(query, sort, response).then(() => { console.log('updated response') }).catch((e) => {
+    // //     console.log(e);
+    // // });
+}
