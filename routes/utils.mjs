0097ecd995b4b5e7f99df9064e952ff5f0f02227
anandan balasupramanian <anandan.bs@gmail.com>
2023-03-13 14:53:01 +0530

    question numbers

--- routes/utils.mjs
+++ routes/utils.mjs
@@ -17,7 +17,7 @@ export async function getAllQuestions(major) {
         .toArray();
     let activity = await collection.find({ type: "activity" })
         .toArray();
-    const mcqQuestions = shuffle(mcq).splice(0, 24);
+    const mcqQuestions = shuffle(mcq).splice(0, 25);
     const applicationQuestions = shuffle(applications).splice(0, 5);
     const activitesQuestions = shuffle(activity).splice(0, 1);
     return [...mcqQuestions, ...applicationQuestions, ...activitesQuestions];
